97c92d67cee47d5f6509df689061e82f0ccbe55a
douglaskks <douglaszxv2@gmail.com>
2025-02-19 15:38:26 -0300

    Implementar verificação de acesso para vendedores e recurso de email recente no login

--- lib/screens/signin/sign_in_controller.dart
+++ lib/screens/signin/sign_in_controller.dart
@@ -1,8 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:thunderapp/screens/screens_index.dart';
-
 import '../../shared/components/dialogs/default_alert_dialog.dart';
 import '../../shared/constants/style_constants.dart';
 import 'sign_in_repository.dart';
@@ -18,75 +17,86 @@ class SignInController with ChangeNotifier {
   final SignInRepository _repository = SignInRepository();
   String? email;
   String? password;
-  final TextEditingController _emailController =
-      TextEditingController();
-  final TextEditingController _passwordController =
-      TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   String? errorMessage;
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
-  TextEditingController get emailController =>
-      _emailController;
-  TextEditingController get passwordController =>
-      _passwordController;
+  TextEditingController get emailController => _emailController;
+  TextEditingController get passwordController => _passwordController;
   var status = SignInStatus.idle;
+
+  SignInController() {
+    loadSavedEmail();
+  }
+
+  Future<void> saveEmail(String email) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('userEmail', email);
+  }
+
+  Future<void> loadSavedEmail() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedEmail = prefs.getString('userEmail');
+    if (savedEmail != null) {
+      _emailController.text = savedEmail;
+      notifyListeners();
+    }
+  }
+
   void signIn(BuildContext context) async {
     try {
       status = SignInStatus.loading;
       notifyListeners();
 
-      var succ = await _repository.signIn(
+      var result = await _repository.signIn(
         email: _emailController.text,
         password: _passwordController.text,
       );
-      if (succ == 1) {
+
+      if (result == 1) {
+        await saveEmail(_emailController.text);
         status = SignInStatus.done;
-
         notifyListeners();
-
-
-          // ignore: use_build_context_synchronously
-          Navigator.pushReplacementNamed(
-            context, Screens.home);
-
-
-      }
-      else if (succ == 2) {
-
-
+        // ignore: use_build_context_synchronously
+        Navigator.pushReplacementNamed(context, Screens.home);
+      } 
+      else if (result == 2) {
         // ignore: use_build_context_synchronously
         Navigator.pushReplacementNamed(context, Screens.addStore);
-      }
-      else{
+      } 
+      else if (result == 3) {
         showDialog(
-            context:
-            context,
-            builder:
-                (context) =>
-                DefaultAlertDialogOneButton(
-                  title:
-                  'Erro',
-                  body:
-                  'Credenciais inválidas, verifique seus dados',
-                  confirmText:
-                  'Voltar',
-                  onConfirm: () =>
-                      Get.back(),
-                  buttonColor:
-                  kErrorColor,
-                ));
+          context: context,
+          builder: (context) => DefaultAlertDialogOneButton(
+            title: 'Acesso Negado',
+            body: 'Este aplicativo é exclusivo para vendedores.',
+            confirmText: 'Voltar',
+            onConfirm: () => Get.back(),
+            buttonColor: kErrorColor,
+          )
+        );
         status = SignInStatus.error;
-        setErrorMessage(
-            'Credenciais inválidas, verifique seus dados');
-        notifyListeners();
+        setErrorMessage('Você não tem permissão para acessar este aplicativo.');
+      } 
+      else {
+        showDialog(
+          context: context,
+          builder: (context) => DefaultAlertDialogOneButton(
+            title: 'Erro',
+            body: 'Credenciais inválidas, verifique seus dados',
+            confirmText: 'Voltar',
+            onConfirm: () => Get.back(),
+            buttonColor: kErrorColor,
+          )
+        );
+        status = SignInStatus.error;
+        setErrorMessage('Credenciais inválidas, verifique seus dados');
       }
       notifyListeners();
     } catch (e) {
-
-
       status = SignInStatus.error;
-      setErrorMessage(
-          'Credenciais inválidas verifique seus dados');
+      setErrorMessage('Credenciais inválidas, verifique seus dados');
       notifyListeners();
     }
   }
--- lib/screens/signin/sign_in_repository.dart
+++ lib/screens/signin/sign_in_repository.dart
@@ -8,8 +8,8 @@ class SignInRepository {
   final userStorage = UserStorage();
   String userId = "0";
   String userToken = "0";
+  String noAut = 'Este aplicativo é exclusivo para vendedores.';
   
-
   final _dio = Dio();
   Future<int> signIn({
     required String email,
@@ -24,40 +24,57 @@ class SignInRepository {
           'device_name': "PC"
         },
       );
+      
       if (response.statusCode == 200) {
-          if (await userStorage.userHasCredentials()) {
-            await userStorage.clearUserCredentials();
-          }
-          userId = response.data['user']['id'].toString();
-          userToken = response.data['token'].toString();
-          await userStorage.saveUserCredentials(
-              id: userId,
-              nome:
-                  response.data['user']['name'].toString(),
-              token:
-                  userToken,
-              email:
-                  response.data['user']['email'].toString(),
-              );
-          try{
-            Response response = await _dio.get(
-          '$kBaseURL/bancas/agricultores/$userId',
-          options: Options(headers: {
-            "Authorization": "Bearer $userToken"
-          }));
-      if (response.statusCode == 200) {
-        if(response.data["bancas"].isEmpty){
-          return 2;
-        }else{
-          print(response.statusCode);
-          return 1;}
-      }
-          }catch(e){
-            print(e);
-            return 0;
-          }
-      return 1;
+        if (await userStorage.userHasCredentials()) {
+          await userStorage.clearUserCredentials();
+        }
         
+        userId = response.data['user']['id'].toString();
+        userToken = response.data['token'].toString();
+        
+        await userStorage.saveUserCredentials(
+          id: userId,
+          nome: response.data['user']['name'].toString(),
+          token: userToken,
+          email: response.data['user']['email'].toString(),
+        );
+
+        try {
+          Response userResponse = await _dio.get(
+            '$kBaseURL/users/$userId',
+            options: Options(headers: {"Authorization": "Bearer $userToken"}),
+          );
+          
+          if (userResponse.statusCode == 200) {
+            List roles = userResponse.data['user']['roles'];
+            if (roles.isNotEmpty) {
+              int roleId = roles[0]['id'];
+              print('Role ID: $roleId');
+              
+              // Verifica se é vendedor (role 4)
+              if (roleId == 4) {
+                Response bancaResponse = await _dio.get(
+                  '$kBaseURL/bancas/agricultores/$userId',
+                  options: Options(headers: {"Authorization": "Bearer $userToken"})
+                );
+                
+                if (bancaResponse.statusCode == 200) {
+                  if(bancaResponse.data["bancas"].isEmpty) {
+                    return 2; // Vendedor sem banca
+                  }
+                  return 1; // Sucesso - é vendedor com banca
+                }
+              } else {
+                print(noAut);
+                return 3; // Não autorizado - não é vendedor
+              }
+            }
+          }
+        } catch (e) {
+          print(e);
+          return 0;
+        }
       }
     } catch (e) {
       log(e.toString());
@@ -65,5 +82,4 @@ class SignInRepository {
     }
     return 0;
   }
-
 }
